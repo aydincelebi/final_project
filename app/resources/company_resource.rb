7f4a11d48714791810a8cdfb2e4528c3076f1884
anonymous anonymous <aydinocelebi@gmail.com>
2022-03-15 15:30:56 +0000

    Generate graphiti resources for companies

---
+++ app/resources/company_resource.rb
@@ -0,0 +1,13 @@
+class CompanyResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :industry, :string
+  attribute :size, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
