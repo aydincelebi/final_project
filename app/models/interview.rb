cc3818b231cbf30cc66a6744ae550e0bb95bb56a
anonymous anonymous <aydinocelebi@gmail.com>
2022-03-15 15:29:27 +0000

    Validate presence of interviews.status

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -5,6 +5,8 @@ class Interview < ApplicationRecord
 
   # Validations
 
+  validates :status, :presence => true
+
   validates :status, :inclusion => { :in => [ "pending", "rejected", "passed" ]  }
 
   validates :take_home, :inclusion => { :in => [ "0", "1" ]  }
