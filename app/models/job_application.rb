eb00cf66faa57c44baf3f40ba14af38f9caafbe3
anonymous anonymous <aydinocelebi@gmail.com>
2022-03-15 15:29:27 +0000

    Validate presence of job_applications.interest_level

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -5,6 +5,8 @@ class JobApplication < ApplicationRecord
 
   # Validations
 
+  validates :interest_level, :presence => true
+
   validates :interest_level, :numericality => { :equal_to => 1 }
 
   validates :name, :presence => true
