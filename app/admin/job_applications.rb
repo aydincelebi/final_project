262e88baaba3da765c62289bb361366f472f346b
anonymous anonymous <aydinocelebi@gmail.com>
2022-03-15 15:29:41 +0000

    Whitelist columns in JobApplication for ActiveAdmin

--- app/admin/job_applications.rb
+++ app/admin/job_applications.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register JobApplication do
 
+ permit_params :name, :description, :company_id, :interest_level, :platform, :networking, :comments, :applied_on, :heard_back_on, :user_id, :job_type_id, :url
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
