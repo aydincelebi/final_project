9aeddc7d2ec364f274926db8c718b5b1c2905ce2
anonymous anonymous <aydinocelebi@gmail.com>
2022-03-15 15:29:54 +0000

    Whitelist columns in JobCategory for ActiveAdmin

--- app/admin/job_categories.rb
+++ app/admin/job_categories.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register JobCategory do
 
+ permit_params :category_id, :job_application_id, :job_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
