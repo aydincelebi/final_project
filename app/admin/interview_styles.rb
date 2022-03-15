b91638a1da9d55071980f5b3cc34e0f6de2a9103
anonymous anonymous <aydinocelebi@gmail.com>
2022-03-15 15:29:56 +0000

    Whitelist columns in InterviewStyle for ActiveAdmin

--- app/admin/interview_styles.rb
+++ app/admin/interview_styles.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register InterviewStyle do
 
+ permit_params :interview_id, :style_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
